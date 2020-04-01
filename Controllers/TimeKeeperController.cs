2cda5917dc8dfb4e522133b0275d670b963e72ea
pokedev8 <pokedev8@gmail.com>
2020-03-31 17:16:29 -0700

    Add project files.

---
+++ Controllers/TimeKeeperController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestApp.Models;
+
+namespace TestApp.Controllers
+{
+    public class TimeKeeperController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            TimeKeeperModel timeKeeperModel = new TimeKeeperModel {
+                MondayStart = 0.00M
+            };
+            return View("Index", timeKeeperModel);
+        }
+
+        [HttpPost]
+        public IActionResult Index(TimeKeeperModel tkm)
+        {
+            TimeKeeperModel timeKeeperModel = new TimeKeeperModel {
+                MondayStart = tkm.MondayStart
+            };
+
+            return View("Index", timeKeeperModel);
+        }
+
+    }
+}
